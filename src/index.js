5a63714bb7efd48df1c12903ccb9990e7c6520e4
smartinnni <101671821+smartinnni@users.noreply.github.com>
2026-01-29 08:19:10 -0800

    Improved  the UX

--- src/index.js
+++ src/index.js
@@ -22,9 +22,15 @@ function generatePoem(event) {
     let prompt = 
         `User instructions: Generate a poem about ${instructionsInput.value}`;
     let context = 
-        "You are a Poem expert and love to write short poems, using correct spacing between lines. Your mission is to generate a 4 line poem using basic HTML with correct spacing and stanzas, with each new line separate from one another. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem. Do not input the text HTML with any additional characters or a title, only write the poem and signature.";
+        "You are a Poem expert and love to write short poems, using correct spacing between lines. Your mission is to generate a 4 line poem using basic HTML with correct spacing and stanzas, with each new line separate from one another. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and do not include extra margin or padding below it. Do not input the text HTML with any additional characters or a title, only write the poem and signature.";
     let apiURL = 
         `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    // hide the box until a poem is being generated
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    // input loading text
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
 
     console.log("generating poem");
     console.log(`Prompt ${prompt}`);
