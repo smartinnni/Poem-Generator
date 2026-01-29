a606318b090da5e3d33bd1b0d9624d75a0e19737
smartinnni <101671821+smartinnni@users.noreply.github.com>
2026-01-28 17:30:30 -0800

    Added basic form handling and added Typewriter JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("Hello World");
+function generatePoem(event) {
+    event.preventDefault();
+
+    new Typewriter("#poem", {
+    strings: "I’m Yertle the Turtle!",
+    autoStart: true,
+    delay: 1, 
+    cursor: "",
+    });
+    
+    poemElement.innerHTML = "I’m Yertle the Turtle!";
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener('submit', generatePoem);
